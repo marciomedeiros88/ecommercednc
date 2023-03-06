6d066aebdbe229e3810306124c06e923d56e9204
Márcio Medeiros <118381472+marciomedeiros88@users.noreply.github.com>
2023-03-06 18:19:22 -0300

    adicionando função de aparecer o modal

--- src/assets/componentes/Meucarrinho.js
+++ src/assets/componentes/Meucarrinho.js
@@ -5,6 +5,18 @@
     import iconCarrinho from "../imagens/carrinho.svg";
     import imagemCard from "../imagens/xbox.png";
     const MeuCarrinho = () => {
+
+        function aparecermodal(){
+            document.getElementsByClassName('add_cart')[0].addEventListener("click", function(event) {
+                // highlight the mouseover target
+                let but = document.getElementsByClassName('area_modal')
+                but[0].style.visibility = "visible";
+                but[0].style.transition = "all 0.6s";
+
+                
+                
+            });
+            }
     return (
         <>
         <Topo />
@@ -30,12 +42,32 @@
                 <p className="area_color"></p>
             </div>
 
-            <button className="add_cart border10 botao_azul">
+            <button className="add_cart border10 botao_azul" data-bstoggle="modal" data-bs-target="adicionar" onClick={aparecermodal}>
                 <img src={iconCarrinho} alt="" />
                 Adicionar ao carrinho
             </button>
             </div>
+        
         </div>
+        <div className="area_modal">
+        <div className="modal border100" id="adicionar" tabindex="-1" style={{ }} aria-hidden="true">
+            <div className="description">
+            <img src={imagemCard} alt="" />
+            </div>
+            <div className="information">
+                    
+                    <h2 className="text-center fw-light">Quem somos nós? </h2>
+                    <p className="p-3 fw-light text-center">Tudo o que fazemos procura respeitar essa conexão, desde nosso compromisso com o café de melhor qualidade do mundo até a forma como nós interagimos com nossos clientes e nossas comunidades para conduzir nosso negócio de modo responsável.
+                    </p>    
+                    <div>
+                    <button type="button" className="btn-close float-end" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>   
+
+                    
+
+                </div>
+            </div>
+            </div>
         <Rodape />
         </>
     );
